fc2c60dd81fbaf2d94ccc08bb3c93ec3c8499bb5
arrington <arrington@brown.edu>
2020-06-10 21:19:21 +0000

    Put a marker on snoopy island.

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -180,7 +180,21 @@ function submitUsername() {
 }
 
 function createMap() {
-  const map = new google.maps.Map(
-      document.getElementById("map"),
-      {center: {lat: 37.422, lng: -122.084}, zoom: 16});
+    const snoopy = {
+        lat: 25.4924, 
+        lng: 56.3641
+    };
+
+    const map = new google.maps.Map(
+        document.getElementById("map"),
+        {
+            center: snoopy,
+            mapTypeId: 'satellite', 
+            zoom: 18
+        });
+
+    const marker = new google.maps.Marker({
+        position: snoopy,
+        map: map
+    });
 }
